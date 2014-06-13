54bddbf15862f10836ef7c0c918ab5b0c5f282db
Razzi <razzi53@gmail.com>
2014-06-13 11:06:28 +0200

    Initial commit

---
+++ simple/src/main/java/com/medcat/sample/simple/MapsActivity.java
@@ -0,0 +1,106 @@
+package com.medcat.sample.simple;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.net.URL;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+
+public class MapsActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        new MapsTask().execute();
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        // mMap.addMarker(new MarkerOptions().position(new LatLng(-24.6464, 25.9119)).title("Marker"));
+    }
+
+    private static final String ENDPOINT = "http://10.0.2.2:5000/";
+
+    private class MapsTask extends AsyncTask<URL, Integer, Long> {
+        MarkerOptions marker = new MarkerOptions();
+
+        protected Long doInBackground(URL... urls) {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                .url(ENDPOINT)
+                .build();
+            try {
+                Response response = client.newCall(request).execute();
+                Log.i("blahtagblah", "good stuff" + response.body().string());
+            } catch (IOException e) {
+                Log.e("dummy.tag", "failed http" + e);
+            }
+
+            return 0L;
+        }
+
+        protected void onPostExecute(Long result) {
+            mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(-24.6092451, 25.9305045))
+                .title("new marker")
+            );
+        }
+    }
+}
